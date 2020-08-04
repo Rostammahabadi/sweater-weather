9b0bfb051e8edc418c0ac2695e54b1c1ebdba567
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-08-04 11:55:15 -0600

    added method to check for validation of city/state combination

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    render json: ForecastSerializer.new(Weather.new(create_weather_objects.response))
+    if create_weather_objects
+      render json: ForecastSerializer.new(Weather.new(create_weather_objects.response))
+    else
+      render json: {error: "Incorrect city/state combination"}, status: 400
+    end
   end
 
   private
@@ -10,8 +14,13 @@ class Api::V1::ForecastController < ApplicationController
   end
 
   def create_weather_objects
-    facade = MapquestFacade.new(forecast_params)
-    lat_and_long = facade.get_lat_and_long
-    WeatherFacade.new(lat_and_long[:lat], lat_and_long[:lng], "imperial")
+    validation = Validation.new(forecast_params[:location])
+    if validation.response == true
+      facade = MapquestFacade.new(forecast_params)
+      lat_and_long = facade.get_lat_and_long
+      WeatherFacade.new(lat_and_long[:lat], lat_and_long[:lng], "imperial")
+    else
+      return
+    end
   end
 end
