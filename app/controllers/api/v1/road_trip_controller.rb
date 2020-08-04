4fb5862a850160183aa23b97965a7758c9679ef2
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-08-04 09:25:49 -0600

    added code for sad paths

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,7 +1,26 @@
 class Api::V1::RoadTripController < ApplicationController
-  def index
-    from_lat_lng = MapquestFacade.new(params[:origin]).get_lat_and_long
-    to_lat_lng = MapquestFacade.new(params[:destination]).get_lat_and_long
-    binding.pry
+  def create
+    if road_trip_params[:api_key]
+      user = User.find_by(api_key: road_trip_params[:api_key])
+      if !user.nil?
+        render json: RoadTripSerializer.new(create_road_trip)
+      else
+        render json: { error: "Invalid API key"}, status: 401
+      end
+    else
+      render json: {error: "Provide an API Key"}, status: 401
+    end
+  end
+
+  private
+
+  def road_trip_params
+    params.permit(:origin, :destination, :api_key)
+  end
+
+  def create_road_trip
+    from_lat_lng = MapquestFacade.new(params[:origin]).lat_long
+    to_lat_lng = MapquestFacade.new(params[:destination]).lat_long
+    direction = DirectionsFacade.new(road_trip_params[:origin], road_trip_params[:destination],from_lat_lng, to_lat_lng)
   end
 end
