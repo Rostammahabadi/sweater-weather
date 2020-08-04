933e3ccc65c7768d8874b1da66ba96b42e429d66
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-08-04 12:11:11 -0600

    Tested the validation spec with numbers and characters

--- spec/poros/validation_spec.rb
+++ spec/poros/validation_spec.rb
@@ -17,4 +17,14 @@ describe "validation" do
     validation = Validation.new("asdf")
     expect(validation.response).to eq(false)
   end
+  it "returns false if numbers or other types of characters" do
+
+    validation = Validation.new("1234,1234")
+    expect(validation.response).to eq(false)
+  end
+  it "returns false if numbers or other types of character without comma" do
+
+    validation = Validation.new("12341234")
+    expect(validation.response).to eq(false)
+  end
 end
