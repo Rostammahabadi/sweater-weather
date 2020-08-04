1520eb6bf0041e8fe03b34ba1999015d3c044b9c
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-08-04 13:15:05 -0600

    add validation for if there is a user based on the email provided

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
-    if user.authenticate(params[:password])
-      render json: UserSerializer.new(user)
+    if !user.nil?
+      if user.authenticate(params[:password])
+        render json: UserSerializer.new(user)
+      else
+        render json: {error: "Incorrect password" }, status: 400
+      end
     else
-      render json: {error: user.errors}, status: 400
+      render json: {error: "User does not exist"}, status: 400
     end
   end
 end
