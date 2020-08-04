843e8e58c08128ec31eefe679430b11be03c05b5
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-08-04 11:55:52 -0600

    added vcr

--- spec/services/mapquest_directions_service_spec.rb
+++ spec/services/mapquest_directions_service_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe "Mapquest Direction Service" do
-  it "gets distance between two lat and longs" do
+  it "returns distance" do
+  VCR.use_cassette("gets distance between two lat and longs") do
+      service_return = MapquestDirectionService.new.get_distance("39.738453,-104.984853","38.265425,-104.610415")
 
-    service_return = MapquestDirectionService.new.get_distance("39.738453,-104.984853","38.265425,-104.610415")
-
-    expect(service_return).to eq(111.381)
+      expect(service_return).to eq(111.381)
   end
+end
 end
