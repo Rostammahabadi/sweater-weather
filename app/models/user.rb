4056b815f23f1e19791f2b2921f73d62f0074ae9
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-08-04 13:23:21 -0600

    added method to check if email is in correct format

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ require 'securerandom'
 class User < ApplicationRecord
   before_create :add_api_key
   has_secure_password
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :email, uniqueness: true, presence: true
   validates :password, on: :create, presence: true
   private
