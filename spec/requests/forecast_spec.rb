18ecb9b251e5c34b1939889fdc4e1bb47c81777d
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-08-04 13:33:19 -0600

    update tests for api key exposure

--- spec/requests/forecast_spec.rb
+++ spec/requests/forecast_spec.rb
@@ -10,27 +10,27 @@ describe "Forecast request" do
 
       expect(response).to be_successful
       # test for current information
-      expect(json[:data][:attributes][:current][:datetime]).to eq(1596563040)
-      expect(json[:data][:attributes][:current][:temp_high]).to eq(88.81)
-      expect(json[:data][:attributes][:current][:temp_low]).to eq(69.21)
+      expect(json[:data][:attributes][:current][:datetime]).to eq(1596569404)
+      expect(json[:data][:attributes][:current][:temp_high]).to eq(88.25)
+      expect(json[:data][:attributes][:current][:temp_low]).to eq(69.33)
       expect(json[:data][:attributes][:current][:uvi]).to eq(10.89)
-      expect(json[:data][:attributes][:current][:humidity]).to eq(43)
+      expect(json[:data][:attributes][:current][:humidity]).to eq(38)
       expect(json[:data][:attributes][:current][:visibility]).to eq(10000)
-      expect(json[:data][:attributes][:current][:feels_like]).to eq(82.71)
+      expect(json[:data][:attributes][:current][:feels_like]).to eq(87.4)
       expect(json[:data][:attributes][:current][:sunrise]).to eq(1596542579)
       expect(json[:data][:attributes][:current][:sunset]).to eq(1596593424)
-      expect(json[:data][:attributes][:current][:description]).to eq("scattered clouds")
+      expect(json[:data][:attributes][:current][:description]).to eq("few clouds")
       # test for daily information
       expect(json[:data][:attributes][:daily].count).to eq(6)
       expect(json[:data][:attributes][:daily].first[:description]).to eq("moderate rain")
       expect(json[:data][:attributes][:daily].first[:rain]).to eq(4.62)
-      expect(json[:data][:attributes][:daily].first[:temp_max]).to eq(88.81)
-      expect(json[:data][:attributes][:daily].first[:temp_low]).to eq(69.21)
+      expect(json[:data][:attributes][:daily].first[:temp_max]).to eq(88.25)
+      expect(json[:data][:attributes][:daily].first[:temp_low]).to eq(69.33)
       #test for hourly information
       expect(json[:data][:attributes][:hourly].count).to eq(8)
-      expect(json[:data][:attributes][:hourly].first[:datetime]).to eq(1596560400)
-      expect(json[:data][:attributes][:hourly].first[:description]).to eq("scattered clouds")
-      expect(json[:data][:attributes][:hourly].first[:temp]).to eq(81.21)
+      expect(json[:data][:attributes][:hourly].first[:datetime]).to eq(1596567600)
+      expect(json[:data][:attributes][:hourly].first[:description]).to eq("few clouds")
+      expect(json[:data][:attributes][:hourly].first[:temp]).to eq(85.71)
     end
   end
   it "returns incorrect city state pair" do
